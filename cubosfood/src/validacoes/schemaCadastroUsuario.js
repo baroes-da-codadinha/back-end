91ae626dfc9cbd8de4bc457b04157c54009ef8d4
iostsicaro <iostsicaro@gmail.com>
2021-08-02 01:03:47 -0300

    Arquivo de validação yup

--- cubosfood/src/validacoes/schemaCadastroUsuario.js
+++ cubosfood/src/validacoes/schemaCadastroUsuario.js
@@ -3,7 +3,14 @@ const yup = require('./configuracoes');
 const schemaCadastroUsuario = yup.object().shape({
     nome: yup.string().required(),
     email: yup.string().required().email(),
-    senha: yup.string().required().min(5)
+    senha: yup.string().required().min(5),
+    restaurante: yup.object().shape({
+        nome: yup.string().required('Informe o nome do produto.'),
+        idCategoria: yup.number().required('Informe a categoria do produto.'),
+        taxaEntrega: yup.number().required('Informe a taxa de entrega.'),
+        tempoEntregaEmMinutos: yup.number().required('Informe o tempo de entrega.'),
+        valorMinimoPedido: yup.number().required('Informe o valor mínimo do produto.')
+    })
 });
 
 module.exports = schemaCadastroUsuario;
