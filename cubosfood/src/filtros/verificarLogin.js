ecf72e1d9863dea4782b6031b0dc2fddbbdb2ba5
iostsicaro <iostsicaro@gmail.com>
2021-08-02 01:03:09 -0300

    Arquivo middleware, para verificar login

--- cubosfood/src/filtros/verificarLogin.js
+++ cubosfood/src/filtros/verificarLogin.js
@@ -3,26 +3,35 @@ const jwt = require('jsonwebtoken');
 const senhaHash = require('../senhaHash');
 
 const verificarLogin = async (req, res, next) => {
-    const { authorizarion } = req.headers;
+    const { authorization } = req.headers;
 
-    if (!authorizarion) {
+    if (!authorization) {
         return res.status(401).json('Acesso negado.');
     }
 
     try {
-        const token = authorizarion.replace('Bearer ', '').trim();
-
+        const token = authorization.replace('Bearer ', '').trim();
+        
         const { id } = jwt.verify(token, senhaHash);
 
-        const verificarUsuario = await knex('usuario').where({ id: id }).first();
+        const verificarUsuario = await knex('usuarios').where({ id: id }).first();
 
         if (!verificarUsuario) {
             return res.status.json('Usuário não foi encontrado.');
         }
 
+        const verificarRestaurante = await knex('restaurantes').where({ usuario_id: id }).first();
+
+        if (!verificarRestaurante) {
+            return res.status(404).json('Não foi possível encontrar restaurante.');
+        }
+
+        const { ...restaurante } = verificarRestaurante;
+
         const { senha, ...usuario } = verificarUsuario;
 
         req.usuario = usuario;
+        req.restaurante = restaurante;
 
         next();
     } catch (error) {
