6c6d6b5ad3b06bef3c4bd42e3f7d89d8f86c8ff8
RODRIGO NOAL <rodrigonoal@gmail.com>
2021-08-06 20:53:00 -0300

    alterações no package

--- schema.sql
+++ schema.sql
@@ -6,7 +6,8 @@ DROP TABLE IF EXISTS categorias;
 
 CREATE TABLE categorias(
   id serial NOT NULL PRIMARY KEY,
-  nome varchar(30) NOT NULL
+  nome varchar(30) NOT NULL,
+  url_imagem varchar(255)
 );
 
 DROP TABLE IF EXISTS usuarios;
@@ -29,6 +30,7 @@ CREATE TABLE restaurantes (
   taxa_entrega integer NOT NULL DEFAULT 0,
   tempo_entrega_minutos integer NOT NULL DEFAULT 30,
   valor_minimo_pedido integer NOT NULL DEFAULT 0,
+  url_imagem varchar(255),
   FOREIGN KEY (usuario_id) REFERENCES usuarios (id),
   FOREIGN KEY (categoria_id) REFERENCES categorias (id)
 );
@@ -43,21 +45,22 @@ CREATE TABLE produtos (
   preco integer NOT NULL,
   ativo boolean NOT NULL DEFAULT TRUE,
   permite_observacoes boolean NOT NULL DEFAULT FALSE,
+  url_imagem varchar(255),
   FOREIGN KEY (restaurante_id) REFERENCES restaurantes (id)
 );
 
 /* INSERÇÃO MANUAL DE VALORES NAS TABELAS */
 INSERT INTO categorias
-(nome)
+(nome, url_imagem)
 VALUES
-('Diversos'),
-('Lanches'),
-('Carnes'),
-('Massas'),
-('Pizzas'),
-('Japonesa'),
-('Chinesa'),
-('Mexicano'),
-('Brasileira'),
-('Italiana'),
-('Árabe');
+('Diversos', 'https://images.pexels.com/photos/1065030/pexels-photo-1065030.jpeg'),
+('Lanches', 'https://images.pexels.com/photos/534285/pexels-photo-534285.jpeg'),
+('Carnes', 'https://images.pexels.com/photos/2313686/pexels-photo-2313686.jpeg'),
+('Massas', 'https://images.pexels.com/photos/1527603/pexels-photo-1527603.jpeg'),
+('Pizzas', 'https://images.pexels.com/photos/1049626/pexels-photo-1049626.jpeg'),
+('Japonesa', 'https://images.pexels.com/photos/2323398/pexels-photo-2323398.jpeg'),
+('Chinesa', 'https://images.pexels.com/photos/2347311/pexels-photo-2347311.jpeg'),
+('Mexicano', 'https://images.pexels.com/photos/5737247/pexels-photo-5737247.jpeg'),
+('Brasileira', 'https://images.pexels.com/photos/5695872/pexels-photo-5695872.jpeg'),
+('Italiana', 'https://images.pexels.com/photos/41320/beef-cheese-cuisine-delicious-41320.jpeg'),
+('Árabe', 'https://images.pexels.com/photos/4374580/pexels-photo-4374580.jpeg');
