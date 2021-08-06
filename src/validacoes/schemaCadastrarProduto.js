bf17a20706b36ee058c77820ffe33191cbce6a07
RODRIGO NOAL <rodrigonoal@gmail.com>
2021-08-06 17:09:20 -0300

    tarefa: limit json express corrigido

--- src/validacoes/schemaCadastrarProduto.js
+++ src/validacoes/schemaCadastrarProduto.js
@@ -3,7 +3,8 @@ const yup = require('./configuracoes');
 const schemaCadastroProduto = yup.object().shape({
     nome: yup.string().required(),
     preco: yup.number().required(),
-    permiteObservacoes: yup.string().required()
+    permiteObservacoes: yup.string().required(),
+    urlImagem: yup.string().required(),
 });
 
 module.exports = schemaCadastroProduto;
